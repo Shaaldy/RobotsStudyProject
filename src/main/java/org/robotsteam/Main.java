0bed406587ee32a2235bc7e46ffeb78b39292b0f
Shaaldy <shaldeeman@gmail.com>
2024-05-11 18:15:03 +0500

    Переделка под Inellij Idea

---
+++ src/main/java/org/robotsteam/Main.java
@@ -0,0 +1,28 @@
+
+package org.robotsteam;
+
+import org.robotsteam.gui.MainApplicationFrame;
+
+import java.awt.Frame;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main
+{
+  public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+//        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    SwingUtilities.invokeLater(() -> {
+      MainApplicationFrame frame = new MainApplicationFrame();
+      frame.pack();
+      frame.setVisible(true);
+      frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+    });
+  }}
--- src/main/java/org/robotsteam/log/LogLevel.java
+++ src/main/java/org/robotsteam/log/LogLevel.java
@@ -1,4 +1,4 @@
-package log;
+package org.robotsteam.log;
 
 public enum LogLevel
 {
@@ -8,17 +8,16 @@ public enum LogLevel
     Warning(3),
     Error(4),
     Fatal(5);
-    
+
     private int m_iLevel;
-    
+
     private LogLevel(int iLevel)
     {
         m_iLevel = iLevel;
     }
-    
+
     public int level()
     {
         return m_iLevel;
     }
 }
-
